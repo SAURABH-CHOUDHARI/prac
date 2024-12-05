1a97cea6d55cb6564fd6b4c76cb9bb745be5a4ca
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2024-12-05 16:35:10 +0530

    tailwind

--- dom task/ankur clock analogue/script.js
+++ dom task/ankur clock analogue/script.js
@@ -1,40 +1,22 @@
 // Get DOM elements
-const sec = document.querySelector(".dclock .sec");
-const min = document.querySelector(".dclock .min");
-const hour = document.querySelector(".dclock .hour");
+const sec = document.querySelector(" .sec");
+const min = document.querySelector(".min");
+const hour = document.querySelector(".hour");
 
-let now = new Date();
-let h = now.getHours();
-let m = now.getMinutes();
-let s = now.getSeconds();
 
 function updateClockHands() {
-    const secondsDegrees = s * 6; 
-    const minutesDegrees = m * 6 + (s / 60) * 6; 
-    const hoursDegrees = h * 30 + (m / 60) * 30; 
+    const currentDate = new Date();
+    const seconds = currentDate.getSeconds();
+    const minutes = currentDate.getMinutes();
+    const hours = currentDate.getHours();
 
-    sec.style.transform = `rotateZ(${secondsDegrees}deg)`;
-    min.style.transform = `rotateZ(${minutesDegrees}deg)`;
-    hour.style.transform = `rotateZ(${hoursDegrees}deg)`;
+    const secondsDegrees = seconds * 6; 
+    const minutesDegrees = minutes * 6 + (seconds / 60) * 6; 
+    const hoursDegrees = (hours % 12) * 30 + (minutes / 60) * 30; 
+
+    sec.style.rotate = `${secondsDegrees}deg`;
+    min.style.rotate = `${minutesDegrees}deg`;
+    hour.style.rotate = `${hoursDegrees}deg`;
 }
 
-function loop() {
-    s++;
-    if (s >= 60) {
-        s = 0;
-        m++;
-        if (m >= 60) {
-            m = 0;
-            h++;
-            if (h >= 24) {
-                h = 0;
-            }
-        }
-    }
-    
-    updateClockHands();
-}
-
-updateClockHands();
-
-setInterval(loop, 1000);
+setInterval(updateClockHands, 1000);
